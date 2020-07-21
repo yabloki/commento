accf86570a90aab603c475232fbb22e7d8b160c8
Kirill Moizik <moizik@gmail.com>
2020-07-21 19:58:26 -0400

    fix cors, try

--- api/router.go
+++ api/router.go
@@ -25,7 +25,7 @@ func routesServe() error {
 	}
 
 	origins := handlers.AllowedOrigins([]string{"http://core.2cents.media", "http://2cents.media"})
-	headers := handlers.AllowedHeaders([]string{"X-Requested-With"})
+	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "content-type"})
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"})
 
 	addrPort := os.Getenv("BIND_ADDRESS") + ":" + os.Getenv("PORT")
